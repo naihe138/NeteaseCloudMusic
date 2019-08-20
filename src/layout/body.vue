8cfbbd4bed9962e8d7bfad3c89227413d69e5d5f
何文林 <370215230@qq.com>
2019-08-20 15:30:34 +0800

    scroll style

--- src/layout/body.vue
+++ src/layout/body.vue
@@ -38,6 +38,18 @@
       overflow-y: auto;
       box-sizing: border-box;
       position: relative;
+      &::-webkit-scrollbar-track {
+        // border: 1px solid black;
+        background-color: #FFF;
+      }
+      &::-webkit-scrollbar {
+        width: 8px;
+        background-color: #F5F5F5;
+      }
+      &::-webkit-scrollbar-thumb {
+        background-color: #e1e1e1;
+        border-radius: 6px;
+      }
     }
   }
 </style>
