2fb857b7cf4753ab1ba804e280391a4d80a635aa
何文林 <370215230@qq.com>
2019-08-23 15:28:48 +0800

    detail

---
+++ src/views/anchor-radio/detail.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <DHeader />
+  </section>
+</template>
+
+<script>
+  import DHeader from './components/detail/heander'
+  export default {
+    components: {
+      DHeader
+    }
+  }
+</script>
