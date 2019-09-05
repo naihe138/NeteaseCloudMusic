7773c30aac82308cd5e620dcd0031f8725e5cbc5
hewenlin01 <370215230@qq.com>
2019-09-05 16:36:09 +0800

    ui

--- src/views/song-list/components/detail/comment.vue
+++ src/views/song-list/components/detail/comment.vue
@@ -6,6 +6,14 @@
         <div class="textCount">140</div>
       </div>
     </div>
+    <div class="submit">
+      <div class="iconbox">
+        <i class="iconfont iconbiaoqing"></i>
+        <i class="iconfont iconat"></i>
+        <i class="iconfont iconiconjing"></i>
+      </div>
+      <div class="btn">评论</div>
+    </div>
   </div>
 </template>
 
@@ -29,7 +37,19 @@
     overflow-x: hidden;
     overflow: auto;
     display: grid;
-    grid-template-columns: auto 32px;
+    grid-template-columns: auto 30px;
+    &::-webkit-scrollbar-track {
+      // border: 1px solid black;
+      background-color: #FFF;
+    }
+    &::-webkit-scrollbar {
+      width: 8px;
+      background-color: #F5F5F5;
+    }
+    &::-webkit-scrollbar-thumb {
+      background-color: #e1e1e1;
+      border-radius: 6px;
+    }
     .textarea {
       resize: none;
       outline: none;
@@ -40,13 +60,30 @@
       word-wrap: break-word;
     }
     .textCount {
-      width: 32px;
+      width: 30px;
       position: sticky;
       height: 56px;
       top: 1px;
       display: flex;
       align-items: flex-end;
       color: #cccccc;
+    }
+  }
+  .submit {
+    margin-top: 15px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .iconbox {
+      i {
+        color: #666666;
+        margin-right: 10px;
+      }
+    }
+    .btn {
+      border: 1px solid #d9d9d9;
+      padding: 4px 12px;
+      border-radius: 16px;
     }
   }
 </style>
