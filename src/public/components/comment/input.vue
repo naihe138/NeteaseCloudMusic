7f4f947d9d7e59f572881b0efbcd7bfa6d28fe5a
hewenlin01 <370215230@qq.com>
2019-09-05 16:46:32 +0800

    ii

---
+++ src/public/components/comment/input.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <div class="inputwrap">
+      <div class="inputbox">
+        <div contenteditable="true" class="textarea" @input="handleInput"></div>
+        <div class="textCount">{{count}}</div>
+      </div>
+    </div>
+    <div class="submit">
+      <div class="iconbox">
+        <i class="iconfont iconbiaoqing"></i>
+        <i class="iconfont iconat"></i>
+        <i class="iconfont iconiconjing"></i>
+      </div>
+      <div class="btn">评论</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  const COUNT = 140
+  export default {
+    data () {
+      return {
+        count: COUNT
+      }
+    },
+    methods: {
+      handleInput (e) {
+        this.count = COUNT - e.target.innerText.length
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .inputwrap {
+    height: 60px;
+    border: 1px solid #e6e6e6;
+    overflow: hidden;
+    border-radius: 4px;
+    padding: 5px 0;
+  }
+  .inputbox {
+    width: 100%;
+    box-sizing: border-box;
+    height: 60px;
+    position: relative;
+    overflow-x: hidden;
+    overflow: auto;
+    display: grid;
+    grid-template-columns: auto 30px;
+    &::-webkit-scrollbar-track {
+      background-color: #FFF;
+    }
+    &::-webkit-scrollbar {
+      width: 8px;
+      background-color: #F5F5F5;
+    }
+    &::-webkit-scrollbar-thumb {
+      background-color: #e1e1e1;
+      border-radius: 6px;
+    }
+    .textarea {
+      resize: none;
+      outline: none;
+      margin: 0;
+      box-sizing: border-box;
+      padding-left: 10px;
+      word-break: break-all;
+      word-wrap: break-word;
+    }
+    .textCount {
+      width: 30px;
+      position: sticky;
+      height: 56px;
+      top: 1px;
+      display: flex;
+      align-items: flex-end;
+      color: #cccccc;
+    }
+  }
+  .submit {
+    margin-top: 15px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .iconbox {
+      i {
+        color: #666666;
+        margin-right: 10px;
+      }
+    }
+    .btn {
+      border: 1px solid #d9d9d9;
+      padding: 4px 12px;
+      border-radius: 16px;
+    }
+  }
+</style>
--- src/views/song-list/components/detail/comment.vue
+++ src/views/song-list/components/detail/comment.vue
@@ -1,89 +1,20 @@
 <template>
   <div class="comment">
-    <div class="inputwrap">
-      <div class="inputbox">
-        <div contenteditable="true" class="textarea"></div>
-        <div class="textCount">140</div>
-      </div>
-    </div>
-    <div class="submit">
-      <div class="iconbox">
-        <i class="iconfont iconbiaoqing"></i>
-        <i class="iconfont iconat"></i>
-        <i class="iconfont iconiconjing"></i>
-      </div>
-      <div class="btn">评论</div>
-    </div>
+    <CommentInput />
   </div>
 </template>
+
+<script>
+  import CommentInput from '@/public/components/comment/input.vue'
+  export default {
+    components: {
+      CommentInput
+    }
+  }
+</script>
 
 <style lang="scss" scoped>
   .comment {
     margin: 10px 20px 20px 30px;
-  }
-  .inputwrap {
-    height: 60px;
-    border: 1px solid #e6e6e6;
-    overflow: hidden;
-    border-radius: 4px;
-    padding: 5px 0;
-  }
-  .inputbox {
-    width: 100%;
-    // border: 1px solid #e6e6e6;
-    box-sizing: border-box;
-    height: 60px;
-    position: relative;
-    overflow-x: hidden;
-    overflow: auto;
-    display: grid;
-    grid-template-columns: auto 30px;
-    &::-webkit-scrollbar-track {
-      // border: 1px solid black;
-      background-color: #FFF;
-    }
-    &::-webkit-scrollbar {
-      width: 8px;
-      background-color: #F5F5F5;
-    }
-    &::-webkit-scrollbar-thumb {
-      background-color: #e1e1e1;
-      border-radius: 6px;
-    }
-    .textarea {
-      resize: none;
-      outline: none;
-      margin: 0;
-      box-sizing: border-box;
-      padding-left: 10px;
-      word-break: break-all;
-      word-wrap: break-word;
-    }
-    .textCount {
-      width: 30px;
-      position: sticky;
-      height: 56px;
-      top: 1px;
-      display: flex;
-      align-items: flex-end;
-      color: #cccccc;
-    }
-  }
-  .submit {
-    margin-top: 15px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    .iconbox {
-      i {
-        color: #666666;
-        margin-right: 10px;
-      }
-    }
-    .btn {
-      border: 1px solid #d9d9d9;
-      padding: 4px 12px;
-      border-radius: 16px;
-    }
   }
 </style>
