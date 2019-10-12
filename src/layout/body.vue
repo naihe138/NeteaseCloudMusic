79449df2f6d5abd6b2707f33cd2abb0fa72f5267
naice138 <370215230@qq.com>
2019-10-12 15:11:13 +0800

    scroll

--- src/layout/body.vue
+++ src/layout/body.vue
@@ -34,6 +34,7 @@
 
 
 <style lang="scss" scoped>
+  @import "../public/scss/comment.scss";
   .m-body {
     background-color: #ffffff;
     display: flex;
@@ -51,18 +52,7 @@
       overflow-y: auto;
       box-sizing: border-box;
       position: relative;
-      &::-webkit-scrollbar-track {
-        // border: 1px solid black;
-        background-color: #FFF;
-      }
-      &::-webkit-scrollbar {
-        width: 8px;
-        background-color: #F5F5F5;
-      }
-      &::-webkit-scrollbar-thumb {
-        background-color: #e1e1e1;
-        border-radius: 6px;
-      }
+      @include scrollbar();
     }
   }
 </style>
--- src/public/components/player/lyrisc.vue
+++ src/public/components/player/lyrisc.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="lyrisc">
     <div class="title">
-      <h3>不得不爱<i>标准音乐</i></h3>
+      <h4>不得不爱<i>标准音乐</i></h4>
       <p class="info">
         <span>专辑：<a>不得不爱</a></span>
         <span>歌手：<a>不得不爱</a></span>
@@ -9,29 +9,103 @@
       </p>
     </div>
     <div class="lyrisc-wrap">
-      <ul class="content">
-        <li class="row introduce">作曲：阿瑟大神的离开家</li>
-        <li class="row introduce">作曲：阿瑟大神的离开家</li>
-        <li class="row introduce">作曲：阿瑟大神的离开家</li>
-        <li class="row introduce active">作曲：阿瑟大神的离开家</li>
-        <li class="row">
-          <p>all we need know is</p>
-          <p>我们知道的是</p>
-        </li>
-      </ul>
+      <div class="content">
+        <ul class="lyrisc-text">
+          <li class="row introduce">作曲：阿瑟大神的离开家</li>
+          <li class="row introduce">作曲：阿瑟大神的离开家</li>
+          <li class="row introduce">作曲：阿瑟大神的离开家</li>
+          <li class="row introduce active">作曲：阿瑟大神的离开家</li>
+          <li class="row">
+            <p>all we need know is</p>
+            <p>我们知道的是</p>
+          </li>
+          <li class="row introduce">作曲：阿瑟大神的离开家</li>
+          <li class="row introduce active">作曲：阿瑟大神的离开家</li>
+          <li class="row">
+            <p>all we need know is</p>
+            <p>我们知道的是</p>
+          </li>
+          <li class="row introduce">作曲：阿瑟大神的离开家</li>
+          <li class="row introduce active">作曲：阿瑟大神的离开家</li>
+          <li class="row">
+            <p>all we need know is</p>
+            <p>我们知道的是</p>
+          </li>
+        </ul>
+        <div class="divice"></div>
+      </div>
       <div class="control"></div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scopeed>
+  @import "../../scss/comment.scss";
   .lyrisc {
     flex: 1;
     margin-top: 36px;
   }
   .title {
-    h3{
-      font-size: 24px;
+    h4{
+      font-size: 20px;
+      font-weight: 400;
+      display: flex;
+      align-items: center;
+      i {
+        display: inline-block;
+        color: #cf3d3c;
+        border: 1px solid #cf3d3c;
+        font-size: 10px;
+        border-radius: 2px;
+        margin-left: 2px;
+        padding: 1px 4px;
+      }
+    }
+    .info {
+      margin: 20px 0 30px 0;
+      display: flex;
+      color: #646464;
+      span {
+        margin-right: 40px;
+      }
+      a {
+        color: #5881ab;
+      }
+    }
+  }
+  .lyrisc-wrap {
+    display: flex;
+    height: 342px;
+    .content {
+      width: 332px;
+      height: 342px;
+      position: relative;
+      .divice {
+        width: 1px;
+        height: 342px;
+        position: absolute;
+        background: #eeedee;
+        top: 0;
+        right: 0px;
+      }
+      .lyrisc-text {
+        width: 332px;
+        height: 342px;
+        overflow-x: hidden;
+        overflow-y: auto;
+        @include scrollbar(#faf9fa, #faf9fa);
+      }
+      li {
+        margin-top: 20px;
+        color: #646464;
+        font-size: 14px;
+      }
+      li.active{
+        color: #000000;
+      }
+    }
+    .control {
+      width: 30px;
     }
   }
 </style>
